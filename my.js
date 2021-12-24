d4f62183538e6a080b311934a328ca57cf7a9747
csmaejo <izebearwhite@gmail.com>
2021-12-24 17:32:37 +0700

    first

---
+++ my.js
@@ -0,0 +1,18 @@
+var example = new Vue({
+    el: "#example-1",
+    data:{
+        name:"bunyanuch",
+        X:"SHOPPING",
+        V:"MOVIE",
+        B:"MUSIC",
+        M:"READ A NOVEL",
+        W:"1 HOUR",
+        E:"2 HOUR",
+        R:"10 HOUR",
+        T:"20 HOUR",
+        A:"SPENDING",
+        S:"SURF THE INTERNET"
+
+    },
+    
+})
