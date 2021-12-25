40320351214e76fffb2fcbf5054c0bf684f1ba96
csmaejo <bunyanuch.phu@damrong.ac.th>
2021-12-25 15:34:32 +0700

    first commit

--- my.js
+++ my.js
@@ -1,17 +1,23 @@
 var example = new Vue({
     el: "#example-1",
     data:{
-        name:"bunyanuch",
-        X:"SHOPPING",
-        V:"MOVIE",
-        B:"MUSIC",
-        M:"READ A NOVEL",
-        W:"1 HOUR",
-        E:"2 HOUR",
-        R:"10 HOUR",
-        T:"20 HOUR",
-        A:"SPENDING",
-        S:"SURF THE INTERNET"
+        name:"Activity",
+        X:"10 HOUR ",
+        V:"1 HOUR ",
+        B:"2 HOUR ",
+        M:"10 HOUR ",
+        W:"SOCIAL",
+        E:"MOVIE",
+        R:"MUSIC",
+        T:"READ A NOVEL",
+        A:"period of use",
+        S:"travel",
+        p:"BUNYANUCH PHUTEMATAWUT",
+        l:"I'M PLXM",
+        c:"Future",
+        D:"31 Dec 2:00 pm",
+
+        
 
     },
     
